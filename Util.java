6fa1f5d4b5ee90bfbb3a3efc6c05ef26513903be
yiannisd1 <69043387+yiannisd1@users.noreply.github.com>
2022-05-20 06:39:29 +1000

    Util Class

---
+++ Util.java
@@ -0,0 +1,16 @@
+package com.example.lostandfoundappfinal.util;
+
+public class Util {
+    
+    public static final int DATABASE_VERSION = 1;
+    public static final String DATABASE_NAME = "user_db";
+    public static final String TABLE_NAME = "users";
+    public static final String USER_ID = "user_id";
+
+    public static final String TYPEOFADVERT = "typeOfAdvert";
+    public static final String NAME = "name";
+    public static final String PHONE = "phone";
+    public static final String DESCRIPTION = "description";
+    public static final String DATE = "date";
+    public static final String LOCATION = "location";
+}
